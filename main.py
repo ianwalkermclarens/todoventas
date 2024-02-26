034d7179e96ae6fd97b9730b4472b6f7bd481bd3
ianwalkermclarens <32206406+ianwalkermclarens@users.noreply.github.com>
2024-02-25 22:02:52 -0600

    more

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from PyQt6.QtWidgets import QApplication
 from gui.login import Login
+import sys
 
 class AppMain():
     def __init__(self):
-        self.app = QApplication([])
+        self.app = QApplication(sys.argv)
         self.login = Login()
         super().__init__()
         self.app.exec()
